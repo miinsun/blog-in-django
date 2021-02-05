0361fda03891c8566d817880d819658ae9f45c46
minsun <kor3334@naver.com>
2021-02-05 20:49:52 +0900

    update login

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = '/'
