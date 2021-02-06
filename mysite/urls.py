579bb1e44c8d4c9e24233e898d35435012e27eeb
minsun <kor3334@naver.com>
2021-02-06 20:28:53 +0900

    로그아웃 페이지 추가

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,11 +18,14 @@ from django.contrib import admin
 from django.urls import path
 
 from django.contrib.auth import views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     # url(r'^accounts/login/$', views.login, name='login'),
     url( r'^login/$', views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    # url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
+    url( r'^logout/$', views.LogoutView.as_view(template_name="registration/logout.html"), name="logout", kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
